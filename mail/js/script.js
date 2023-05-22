61256e4e6878261cce7a0af121baead1b4df3577
SimoneVilardo <simovila95@gmail.com>
2023-05-22 14:55:51 +0200

    Esercizio mail terminato

---
+++ mail/js/script.js
@@ -0,0 +1,17 @@
+let mail = ['simone@gmail.com', 'gianni@gmail.com', 'filippo@gmail.com', 'dario@gmail.com', 'daniele@gmail.com', 'chiara@gmail.com', 'marta@gmail.com', 'marco@gmail.com', 'giovanni@gmail.com', 'davide@gmail.com'];
+
+let utente = prompt('Inserisci la tua mail');
+let flag = false;
+
+for(let i=0; i<mail.length; i++){
+    if(mail[i].toLowerCase() === utente.toLowerCase()){
+        flag = true;
+    }
+}
+
+if(flag === true){
+    console.log('Elemento trovato');
+}
+else{
+    console.log('Elemento non trovato');
+}
